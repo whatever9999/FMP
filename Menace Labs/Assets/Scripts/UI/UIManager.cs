03eecf1cb9c23ec5f8a3e979908b33bd77b6d18a
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2022-01-26 15:06:32 +0000

    Set up tooltips and functions to edit them

--- Menace Labs/Assets/Scripts/UI/UIManager.cs
+++ Menace Labs/Assets/Scripts/UI/UIManager.cs
@@ -7,6 +7,12 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI nameText;
 
+    [SerializeField] private TextMeshProUGUI goalNameText;
+    [SerializeField] private TextMeshProUGUI goalDescriptionText;
+    [SerializeField] private TextMeshProUGUI goalStoryText;
+
+    [SerializeField] private TextMeshProUGUI ageTooltipText;
+
     void Start()
     {
         instance = this;
@@ -22,5 +28,19 @@ public class UIManager : MonoBehaviour
     private void SetCloneName()
     {
         nameText.text = "Clone #" + Random.Range(0, 9999).ToString();
+    }
+
+    // TODO: Take in goal info to add to tooltip
+    private void SetGoal()
+    {
+        goalNameText.text = "Goal Name";
+        goalDescriptionText.text = "Goal Description";
+        goalStoryText.text = "Goal Story";
+    }
+
+    // TODO: Get max number of days from the AgeManager
+    private void SetAgeTooltip(int age)
+    {
+        ageTooltipText.text = age.ToString() + "/6 days remaining";
     }
 }
