4918b639a9d519c02c0cd74bc9af4a05ca52a682
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2022-01-27 02:30:39 +0000

    Added camera movement

---
+++ Menace Labs/Assets/Scripts/Camera.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float lookSpeed = 10.0f;
+    public float moveSpeed = 10.0f;
+    public float zoomSpeed = 8.0f;
+    public float edgeScrollSize = 10.0f;
+    public float edgeScrollSpeed = 1.0f;
+
+    private static string mouseXString = "Mouse X";
+    private static string mouseYString = "Mouse Y";
+    private static string verticalString = "Vertical";
+    private static string horizontalString = "Horizontal";
+
+    private float rotateX, rotateY, moveVertical, moveHorizontal, moveY;
+
+    public void CheckInput()
+    {
+        // Reset values
+        rotateX = 0.0f;
+        rotateY = 0.0f;
+        moveVertical = 0.0f;
+        moveHorizontal = 0.0f;
+        moveY = 0.0f;
+
+        // Move camera on middle mouse
+        if (Input.GetMouseButton(2))
+        {
+            rotateX = Input.GetAxis(mouseXString) * lookSpeed;
+            rotateY = Input.GetAxis(mouseYString) * lookSpeed;
+        }
+
+        // Edge Scrolling
+        if (Input.mousePosition.x > Screen.width - edgeScrollSize)
+        {
+            moveHorizontal += edgeScrollSpeed;
+        }
+        if (Input.mousePosition.x < edgeScrollSize)
+        {
+            moveHorizontal -= edgeScrollSpeed;
+        }
+        if (Input.mousePosition.y > Screen.height - edgeScrollSize)
+        {
+            moveY += edgeScrollSpeed;
+        }
+        if (Input.mousePosition.y < edgeScrollSize)
+        {
+            moveY -= edgeScrollSpeed;
+        }
+
+        // Middle mouse scroll also moves vertically
+        moveVertical += Input.GetAxis(verticalString) + (Input.mouseScrollDelta.y * zoomSpeed);
+        moveHorizontal += Input.GetAxis(horizontalString);
+    }
+
+    private void Update()
+    {
+        CheckInput();
+
+        bool moved = (rotateX != 0.0f) || (rotateY != 0.0f) || (moveVertical != 0.0f) || (moveHorizontal != 0.0f) || (moveY != 0.0f);
+        if (moved)
+        {
+            // ROTATE
+            float rotationX = transform.localEulerAngles.x;
+            float newRotationY = transform.localEulerAngles.y + rotateX;
+
+            // Clamp
+            float newRotationX = (rotationX - rotateY);
+            if (rotationX <= 90.0f && newRotationX >= 0.0f)
+            {
+                newRotationX = Mathf.Clamp(newRotationX, 0.0f, 90.0f);
+            }
+            if (rotationX >= 270.0f)
+            {
+                newRotationX = Mathf.Clamp(newRotationX, 270.0f, 360.0f);
+            }
+
+            transform.localRotation = Quaternion.Euler(newRotationX, newRotationY, transform.localEulerAngles.z);
+
+            // MOVE
+            float finalMoveSpeed = moveSpeed * Time.deltaTime;
+            transform.position += transform.forward * finalMoveSpeed * moveVertical;
+            transform.position += transform.right * finalMoveSpeed * moveHorizontal;
+            transform.position += transform.up * finalMoveSpeed * moveY;
+        }
+    }
+}
