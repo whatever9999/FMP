24b2dd61c42a2f1dbd9cd8b3cafa485e68871d32
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2022-02-02 12:16:29 +0000

    Add animations

--- Menace Labs/Assets/Scripts/AnimationManager.cs
+++ Menace Labs/Assets/Scripts/AnimationManager.cs
@@ -8,46 +8,81 @@ public class AnimationManager : MonoBehaviour
         IDLE,
         WALKING,
         DANCING,
-        DYING,
-        BOREDOM,
-        REFUSE,
-        REACT,
-        CLEANING,
-        FIXING,
+        BORED,
+        CLEAN_PUDDLE,
+        CLEAN_OBJECT,
         USE_FRIDGE,
-        SIT,
+        DIE,
+        EATING,
+        FIXING,
+        USE_OVEN,
+        REFUSE,
         PLAY_GAMES,
-        PHONE_CALL,
-        USE_TOILET,
+        REACT,
         SHOWER,
         SLEEP,
-        USE_OVEN,
-        USE_CAMERA,
-        PLAY_POOL,
-        USE_COMPUTER,
-        MOP_PUDDLE,
+        TYPE,
+        PLAY_DARTS,
         TIDY_RUBBISH,
-        EAT,
+        USE_CAMERA,
+        USE_PHONE,
+        SIT,
         NUM_ANIMATION_TYPES,
     }
 
-    [SerializeField] private AnimationClip idleAnimation;
-    [SerializeField] private AnimationClip walkingAnimation;
-    [SerializeField] private AnimationClip dancingAnimation;
-    [SerializeField] private AnimationClip dyingAnimation;
+    [SerializeField] private AnimationClip[] idleAnimationSet;
+    [SerializeField] private AnimationClip[] walkingAnimationSet;
+    [SerializeField] private AnimationClip[] dancingAnimationSet;
+    [SerializeField] private AnimationClip[] boredAnimationSet;
+    [SerializeField] private AnimationClip[] cleanPuddleAnimationSet;
+    [SerializeField] private AnimationClip[] cleanObjectAnimationSet;
+    [SerializeField] private AnimationClip[] useFridgeAnimationSet;
+    [SerializeField] private AnimationClip[] dieAnimationSet;
+    [SerializeField] private AnimationClip[] eatingAnimationSet;
+    [SerializeField] private AnimationClip[] fixingAnimationSet;
+    [SerializeField] private AnimationClip[] useOvenAnimationSet;
+    [SerializeField] private AnimationClip[] refuseAnimationSet;
+    [SerializeField] private AnimationClip[] playGamesAnimationSet;
+    [SerializeField] private AnimationClip[] reactAnimationSet;
+    [SerializeField] private AnimationClip[] showerAnimationSet;
+    [SerializeField] private AnimationClip[] sleepAnimationSet;
+    [SerializeField] private AnimationClip[] typeAnimationSet;
+    [SerializeField] private AnimationClip[] playDartsAnimationSet;
+    [SerializeField] private AnimationClip[] tidyRubbishAnimationSet;
+    [SerializeField] private AnimationClip[] useCameraAnimationSet;
+    [SerializeField] private AnimationClip[] usePhoneAnimationSet;
+    [SerializeField] private AnimationClip[] sitAnimationSet;
 
     private Animator animator;
 
-    private Dictionary<AnimationType, AnimationClip> animations = new Dictionary<AnimationType, AnimationClip>();
+    private Dictionary<AnimationType, AnimationClip[]> animations = new Dictionary<AnimationType, AnimationClip[]>();
 
     private void Start()
     {
         animator = GetComponent<Animator>();
 
-        animations.Add(AnimationType.IDLE, idleAnimation);
-        animations.Add(AnimationType.WALKING, walkingAnimation);
-        animations.Add(AnimationType.DANCING, dancingAnimation);
-        animations.Add(AnimationType.DYING, dyingAnimation);
+        animations.Add(AnimationType.IDLE, idleAnimationSet);
+        animations.Add(AnimationType.WALKING, walkingAnimationSet);
+        animations.Add(AnimationType.DANCING, dancingAnimationSet);
+        animations.Add(AnimationType.BORED, boredAnimationSet);
+        animations.Add(AnimationType.CLEAN_PUDDLE, cleanPuddleAnimationSet);
+        animations.Add(AnimationType.CLEAN_OBJECT, cleanObjectAnimationSet);
+        animations.Add(AnimationType.USE_FRIDGE, useFridgeAnimationSet);
+        animations.Add(AnimationType.DIE, dieAnimationSet);
+        animations.Add(AnimationType.EATING, eatingAnimationSet);
+        animations.Add(AnimationType.FIXING, fixingAnimationSet);
+        animations.Add(AnimationType.USE_OVEN, useOvenAnimationSet);
+        animations.Add(AnimationType.REFUSE, refuseAnimationSet);
+        animations.Add(AnimationType.PLAY_GAMES, playGamesAnimationSet);
+        animations.Add(AnimationType.REACT, reactAnimationSet);
+        animations.Add(AnimationType.SHOWER, showerAnimationSet);
+        animations.Add(AnimationType.SLEEP, sleepAnimationSet);
+        animations.Add(AnimationType.TYPE, typeAnimationSet);
+        animations.Add(AnimationType.PLAY_DARTS, playDartsAnimationSet);
+        animations.Add(AnimationType.TIDY_RUBBISH, tidyRubbishAnimationSet);
+        animations.Add(AnimationType.USE_CAMERA, useCameraAnimationSet);
+        animations.Add(AnimationType.USE_PHONE, usePhoneAnimationSet);
+        animations.Add(AnimationType.SIT, sitAnimationSet);
     }
 
     public void SetAnimation(AnimationType type, bool enable)
@@ -70,14 +105,76 @@ public class AnimationManager : MonoBehaviour
             case AnimationType.DANCING:
                 animator.SetBool("Dancing", enable);
                 break;
+            case AnimationType.BORED:
+                if (enable) animator.SetTrigger("Bored");
+                break;
+            case AnimationType.CLEAN_PUDDLE:
+                animator.SetBool("Clean_Puddle", enable);
+                break;
+            case AnimationType.CLEAN_OBJECT:
+                animator.SetBool("Clean_Object", enable);
+                break;
+            case AnimationType.USE_FRIDGE:
+                if (enable) animator.SetTrigger("Use_Fridge");
+                break;
+            case AnimationType.DIE:
+                if (enable) animator.SetTrigger("Die");
+                break;
+            case AnimationType.EATING:
+                animator.SetBool("Eating", enable);
+                break;
+            case AnimationType.FIXING:
+                animator.SetBool("Fixing", enable);
+                break;
+            case AnimationType.USE_OVEN:
+                animator.SetBool("Use_Oven", enable);
+                break;
+            case AnimationType.REFUSE:
+                if (enable) animator.SetTrigger("Refuse");
+                break;
+            case AnimationType.PLAY_GAMES:
+                animator.SetBool("Play_Games", enable);
+                break;
+            case AnimationType.REACT:
+                if (enable) animator.SetTrigger("React");
+                break;
+            case AnimationType.SHOWER:
+                animator.SetBool("Shower", enable);
+                break;
+            case AnimationType.SLEEP:
+                animator.SetBool("Sleep", enable);
+                break;
+            case AnimationType.TYPE:
+                animator.SetBool("Type", enable);
+                break;
+            case AnimationType.PLAY_DARTS:
+                animator.SetBool("Play_Darts", enable);
+                break;
+            case AnimationType.TIDY_RUBBISH:
+                if (enable) animator.SetTrigger("Tidy_Rubbish");
+                break;
+            case AnimationType.USE_CAMERA:
+                animator.SetBool("Use_Camera", enable);
+                break;
+            case AnimationType.USE_PHONE:
+                animator.SetBool("Use_Phone", enable);
+                break;
+            case AnimationType.SIT:
+                animator.SetBool("Sit", enable);
+                break;
         }
     }
 
     public float GetAnimationLength(AnimationType type)
     {
-        AnimationClip animation;
-        animations.TryGetValue(type, out animation);
-        if (animation) return animation.length;
-        return 0.0f;
+        AnimationClip[] animation_set;
+        animations.TryGetValue(type, out animation_set);
+
+        float animation_length = 0.0f;
+        for (int i = 0; i < animation_set.Length; i++)
+        {
+            animation_length += animation_set[i].length;
+        }
+        return animation_length;
     }
 }
