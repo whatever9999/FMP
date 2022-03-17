ed2ccfe2a98abe51d14d97ac6a443c7af55cb0ef
Dominique Russell <30952031+whatever9999@users.noreply.github.com>
2022-03-17 13:10:03 +0000

    Add "loading screen"

--- Menace Labs/Assets/Scripts/UI/MainMenu.cs
+++ Menace Labs/Assets/Scripts/UI/MainMenu.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject loadingScreen;
+
     public void StartButton()
     {
+        loadingScreen.SetActive(true);
         SceneManager.LoadScene(1);
     }
 
