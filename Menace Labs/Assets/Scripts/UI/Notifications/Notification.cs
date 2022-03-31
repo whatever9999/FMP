7e9eefd42a8fdf2e50ee215efe3e1455048de4d8
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2022-03-31 03:38:27 +0100

    #208 Notification Popup Fading

--- Menace Labs/Assets/Scripts/UI/Notifications/Notification.cs
+++ Menace Labs/Assets/Scripts/UI/Notifications/Notification.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class Notification : MonoBehaviour
 {
+    [SerializeField] private float visibleDuration = 10.0f;
+    private float visibleTimer;
+
+    private void Update()
+    {
+        visibleTimer += Time.unscaledDeltaTime;
+
+        if (visibleTimer > visibleDuration)
+        {
+            DeleteNotification();
+        }
+    }
+
     public void DeleteNotification()
     {
         ManagerHandler.instance.NotificationM.DeleteNotification(gameObject);
